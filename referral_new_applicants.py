4cb886a08a6ee724ff06da03c93eb37aa2e9e88b
danimartinlombardo <daniel.martin@cabify.com>
2018-12-19 12:01:41 +0100

    Cohort amplitude: error con auth

--- referral_new_applicants.py
+++ referral_new_applicants.py
@@ -159,12 +159,14 @@ try:
 		FROM
 			(select applicant_id from bp.referral_participants) p
 		''')
-	applicants_string = cur_pg.fetchall()
+	applicants_string_raw = cur_pg.fetchall()
+	applicants_string=[i[0] for i in applicants_string_raw]
 except Exception as e:
 	slack_message(': <!channel> ERROR Unable to prepare applicants string: '+ str(e))
 # try:
-amplitude_payload = "{\"name\":\"GRW_referrals_participants\",\"app_id\":174786,\"id_type\":\"BY_USER_ID\",\"ids\":[\n"+applicants_string+"],\"owner\":\"daniel.martin@cabify.com\",\"published\":true}"
+amplitude_payload = "{\"name\":\"GRW_referrals_participants\",\"app_id\":174786,\"id_type\":\"BY_USER_ID\",\"ids\":[\n"+str(applicants_string)+"],\"owner\":\"daniel.martin@cabify.com\",\"published\":true}"
 response = requests.request("POST", url = 'https://amplitude.com/api/3/cohorts/upload', headers=amplitude_headers, data=amplitude_payload, auth=str(amplitude_auth))
+print(applicants_string)
 print ('Amplitude response: '+response.text)
 # except:
 # 	slack_message(': <!channel> ERROR Unable to update Amplitude cohort')
