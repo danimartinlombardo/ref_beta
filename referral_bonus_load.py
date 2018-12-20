999d0175d07f0289198ab8285ab7ef2008e425ac
danimartinlombardo <daniel.martin@cabify.com>
2018-12-20 12:33:26 +0100

    Push bono conseguido distingue campañas godfather/applicant

--- referral_bonus_load.py
+++ referral_bonus_load.py
@@ -87,7 +87,7 @@ for request in b_requests:
 				braze_payload = "{\n  \"api_key\": \""+braze_api+"\",\n  \"campaign_id\": \"183d9996-4ca0-4987-8cae-f6a344df57b9\",\n  \"recipients\": [\n     {\n      \"external_user_id\": \""+request[3]+"\"\n     }\n   ]\n}"
 				response = requests.post(url = "https://rest.iad-01.braze.com/campaigns/trigger/send", data=braze_payload, headers=braze_headers)
 				print ('Braze response:'+response.text)
-			else request[14] = 'applicant':
+			else:
 				braze_payload = "{\n  \"api_key\": \""+braze_api+"\",\n  \"campaign_id\": \"e65ae958-789b-f2ce-cec0-bca0694489be\",\n  \"recipients\": [\n     {\n      \"external_user_id\": \""+request[3]+"\"\n     }\n   ]\n}"
 				response = requests.post(url = "https://rest.iad-01.braze.com/campaigns/trigger/send", data=braze_payload, headers=braze_headers)
 				print ('Braze response:'+response.text)
