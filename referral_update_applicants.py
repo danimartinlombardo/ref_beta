557fc3d0b2dd310313db219f95523fb2036ad7e3
danimartinlombardo <daniel.martin@cabify.com>
2018-12-20 16:17:42 +0100

    Amplia atributos en braze del padrino y añade al ahijado

--- referral_update_applicants.py
+++ referral_update_applicants.py
@@ -122,51 +122,51 @@ for godfather in braze_arrays:
 		print(': <!channel> ERROR Braze attributes update error')
 
 ###UPDATE APPLICANTS: BRAZE (APPLICANTS)
-try:
-	cur_pg.execute("""	
-		SELECT
-			q.external_id,
-			'"'||string_agg (q.fullname_quote, ', ')||'"' as referrals_godfather_name,
-			'"'||string_agg (q.email_quote, ', ')||'"' as referrals_godfather_email,
-			'"'||string_agg (q.dateline_quote, ', ')||'"' as referrals_applicant_dateline,
-			'"'||string_agg (q.required_do_quote, ', ')||'"' as referrals_applicant_required_do,
-			'"'||string_agg (q.state_quote, ', ')||'"' as referrals_applicant_state,
-			'"'||string_agg (q.actual_do_quote, ', ')||'"' as referrals_applicant_actual_do,
-			'"'||string_agg (q.updated_at_local_quote, ', ')||'"' as referrals_applicant_updated_at_local,
-			'"'||string_agg (q.week_num_quote, ', ')||'"' as referrals_applicant_week_num,
-			'"'||string_agg (q.amount_godfather_quote, ', ')||'"' as referrals_applicant_amount_godfather,
-			'"'||string_agg (q.amount_applicant_quote, ', ')||'"' as referrals_applicant_amount
-		FROM
-			(SELECT
-				rp.applicant_id as external_id,
-				(case when rp.state='clear' then '' else rp.godfather_fullname end ) as fullname_quote,
-				(case when rp.state='clear' then '' else rp.applicant_code end) as email_quote,
-				(case when rp.state='clear' then '' else ''''||(to_char(rp.dateline_dttm,'DD/MM/YYYY'))||'''' end) as dateline_quote,
-				(case when rp.state='clear' then '' else (rp.conditions_required_do)::text end) as required_do_quote,
-				(case when rp.state='clear' then '' else rp.state end) as state_quote,
-				(case when rp.state='clear' then '' else (rp.do_num)::text end) as actual_do_quote,
-				(case when rp.state='clear' then '' else ''''||(to_char(rp.updated_at_local,'DD/MM/YYYY HH:MI'))||'''' end) as updated_at_local_quote,
-				(case when rp.state='clear' then '' else (rp.conditions_week_num)::text end) as week_num_quote,
-				(case when rp.state='clear' then '' else (rp.conditions_amount_granted_godfather)::text end) as amount_godfather_quote,
-				(case when rp.state='clear' then '' else (rp.conditions_amount_granted_applicant)::text end) as amount_applicant_quote
-			FROM
-				bp.referral_participants rp
-			WHERE rp.state != 'obsolete'
-			ORDER BY rp.applicant_id, rp.created_at_utc, rp.applicant_email) q
-		GROUP BY 1;
-	""")
-except psycopg2.Error as e:
-	slack_message(': <!channel> ERROR Unable to create Braze arrays for applicants: '+ str(e))
-	print(': <!channel> ERROR Unable to create Braze arrays for applicants: '+ str(e))
-	exit()
-braze_applicants = cur_pg.fetchall()
-for applicant in braze_applicants:	
-	try:
-		# payload as string
-		braze_payload = "{\n  \"api_key\": \""+braze_api+"\",\n  \"attributes\": [ \n \t{\n \t  \"external_id\":\""+applicant[0]+"\",\n      \"referrals_gf_name_str\": "+applicant[1]+",\n      \"referrals_gf_email_str\": "+applicant[2]+",\n      \"referrals_gf_dateline_str\": "+applicant[3]+",\n      \"referrals_gf_required_do_str\": "+applicant[4]+",\n      \"referrals_gf_state_str\": "+applicant[5]+",\n      \"referrals_gf_actual_do_str\": "+applicant[6]+",\n      \"referrals_gf_updated_at_local_str\": "+applicant[7]+",\n      \"referrals_gf_conditions_week_num_str\": "+applicant[8]+",\n      \"referrals_gf_conditions_godfather_amount_str\": "+applicant[9]+",\n      \"referrals_gf_conditions_applicant_amount_str\": "+applicant[10]+"\n    }\n   ]\n}"
-		response = requests.request("POST", url = "https://rest.iad-01.braze.com/users/track", data=braze_payload, headers=braze_headers)
-		print (applicant[0] + ' Braze attributes updated. Response '+response.text)
-	except:
-		slack_message(': <!channel> ERROR Braze attributes update error')
-		print(': <!channel> ERROR Braze attributes update error')
+# try:
+# 	cur_pg.execute("""	
+# 		SELECT
+# 			q.external_id,
+# 			'"'||string_agg (q.fullname_quote, ', ')||'"' as referrals_godfather_name,
+# 			'"'||string_agg (q.email_quote, ', ')||'"' as referrals_godfather_email,
+# 			'"'||string_agg (q.dateline_quote, ', ')||'"' as referrals_applicant_dateline,
+# 			'"'||string_agg (q.required_do_quote, ', ')||'"' as referrals_applicant_required_do,
+# 			'"'||string_agg (q.state_quote, ', ')||'"' as referrals_applicant_state,
+# 			'"'||string_agg (q.actual_do_quote, ', ')||'"' as referrals_applicant_actual_do,
+# 			'"'||string_agg (q.updated_at_local_quote, ', ')||'"' as referrals_applicant_updated_at_local,
+# 			'"'||string_agg (q.week_num_quote, ', ')||'"' as referrals_applicant_week_num,
+# 			'"'||string_agg (q.amount_godfather_quote, ', ')||'"' as referrals_applicant_amount_godfather,
+# 			'"'||string_agg (q.amount_applicant_quote, ', ')||'"' as referrals_applicant_amount
+# 		FROM
+# 			(SELECT
+# 				rp.applicant_id as external_id,
+# 				(case when rp.state='clear' then '' else rp.godfather_fullname end ) as fullname_quote,
+# 				(case when rp.state='clear' then '' else rp.applicant_code end) as email_quote,
+# 				(case when rp.state='clear' then '' else ''''||(to_char(rp.dateline_dttm,'DD/MM/YYYY'))||'''' end) as dateline_quote,
+# 				(case when rp.state='clear' then '' else (rp.conditions_required_do)::text end) as required_do_quote,
+# 				(case when rp.state='clear' then '' else rp.state end) as state_quote,
+# 				(case when rp.state='clear' then '' else (rp.do_num)::text end) as actual_do_quote,
+# 				(case when rp.state='clear' then '' else ''''||(to_char(rp.updated_at_local,'DD/MM/YYYY HH:MI'))||'''' end) as updated_at_local_quote,
+# 				(case when rp.state='clear' then '' else (rp.conditions_week_num)::text end) as week_num_quote,
+# 				(case when rp.state='clear' then '' else (rp.conditions_amount_granted_godfather)::text end) as amount_godfather_quote,
+# 				(case when rp.state='clear' then '' else (rp.conditions_amount_granted_applicant)::text end) as amount_applicant_quote
+# 			FROM
+# 				bp.referral_participants rp
+# 			WHERE rp.state != 'obsolete'
+# 			ORDER BY rp.applicant_id, rp.created_at_utc, rp.applicant_email) q
+# 		GROUP BY 1;
+# 	""")
+# except psycopg2.Error as e:
+# 	slack_message(': <!channel> ERROR Unable to create Braze arrays for applicants: '+ str(e))
+# 	print(': <!channel> ERROR Unable to create Braze arrays for applicants: '+ str(e))
+# 	exit()
+# braze_applicants = cur_pg.fetchall()
+# for applicant in braze_applicants:	
+# 	try:
+# 		# payload as string
+# 		braze_payload = "{\n  \"api_key\": \""+braze_api+"\",\n  \"attributes\": [ \n \t{\n \t  \"external_id\":\""+applicant[0]+"\",\n      \"referrals_gf_name_str\": "+applicant[1]+",\n      \"referrals_gf_email_str\": "+applicant[2]+",\n      \"referrals_gf_dateline_str\": "+applicant[3]+",\n      \"referrals_gf_required_do_str\": "+applicant[4]+",\n      \"referrals_gf_state_str\": "+applicant[5]+",\n      \"referrals_gf_actual_do_str\": "+applicant[6]+",\n      \"referrals_gf_updated_at_local_str\": "+applicant[7]+",\n      \"referrals_gf_conditions_week_num_str\": "+applicant[8]+",\n      \"referrals_gf_conditions_godfather_amount_str\": "+applicant[9]+",\n      \"referrals_gf_conditions_applicant_amount_str\": "+applicant[10]+"\n    }\n   ]\n}"
+# 		response = requests.request("POST", url = "https://rest.iad-01.braze.com/users/track", data=braze_payload, headers=braze_headers)
+# 		print (applicant[0] + ' Braze attributes updated. Response '+response.text)
+# 	except:
+# 		slack_message(': <!channel> ERROR Braze attributes update error')
+# 		print(': <!channel> ERROR Braze attributes update error')
 slack_message(": Script loaded succesfully. Runtime: %s seconds" % round(time.time() - start_time, 2))
